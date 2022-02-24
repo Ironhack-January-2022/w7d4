31f3596ccd3e00e86fe31f159ea152c1a119a060
Jan Redmann <janredmann1@gmail.com>
2022-02-24 15:14:01 +0100

    add routing

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -5,6 +5,7 @@ import About from "./About";
 import ProjectDetails from "./ProjectDetails";
 import { Projects } from "./Projects";
 import Navbar from "./Navbar";
+import { Routes, Route } from 'react-router-dom';
 
 function NotFound() {
   return <h1>404 - Not Found - 🙀</h1>
@@ -13,9 +14,17 @@ function NotFound() {
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
-      <Projects />
+      <Navbar />
+      <Routes>
+        {/* This is a list of routes -> which url should show which component */}
+        {/* path is the url that the user sees in the browser */}
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/projects/:id' element={<ProjectDetails />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- react-routing/src/ProjectDetails.js
+++ react-routing/src/ProjectDetails.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { myProjects } from './Projects';
+import { useParams } from 'react-router-dom';
 
 export default function ProjectDetails(props) {
 
+	const params = useParams()
+
+	const projectId = params.id
+
+	console.log(projectId)
+
+	const project = myProjects.find(project => project.id === projectId)
+
 	return (
-		<h1>Project Details</h1>
+		<>
+			<h1>Project Details</h1>
+			<h3>Name: {project.name}</h3>
+			<h3>Tech: {project.technologies}</h3>
+			<h4>{project.description}</h4>
+		</>
 	)
 }
